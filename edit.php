af5ea81308b964a79fd2efe339c6287262e029c2
p1613m <p1613m@gmail.com>
2022-12-21 14:50:04 +0500

    Refactor post validate

--- edit.php
+++ edit.php
@@ -18,37 +18,7 @@ if(isset($_POST['submit'])) {
     $content = trim($_POST['content']);
     $image = $_FILES['image'];
 
-    // проверяем отправлен ли файл
-    if($image['size'] > 0) {
-        // объявляем массив с разрешенными типами файлов
-        $types = [
-            'image/jpeg',
-            'image/png',
-            'image/gif',
-        ];
-        // проверяем входит ли тип файла в разрешенные типы
-        if (!in_array($image['type'], $types)) {
-            $errors['image'] = 'Incorrect file type';
-        }
-        // проверяем размер файла в байтах
-        if ($image['size'] > 1 * 1024 * 1024) {
-            $errors['image'] = 'Incorrect image size';
-        }
-    }
-
-    $titleLength = mb_strlen($title);
-    if(!$title || $titleLength > 255) {
-        $errors['title'] = 'Incorrect title';
-    }
-
-    $descriptionLength = mb_strlen($description);
-    if(!$description || $descriptionLength > 500) {
-        $errors['description'] = 'Incorrect description';
-    }
-
-    if(!$content) {
-        $errors['content'] = 'Field is required';
-    }
+    $errors = validatePost($title, $description, $content, $image, true);
 
     if(count($errors) === 0) {
         $imagePath = $post['image_path'];
